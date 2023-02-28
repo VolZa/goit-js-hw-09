2881d59cdd719e387e084cb4bc314350be66b956
VolZa <vvolza@gmail.com>
2023-02-28 18:05:35 +0000

    Deploying to gh-pages from @ VolZa/goit-js-hw-09@b2a8a1de8f872da71005fe4f820b3935ea60db6f 🚀

---
+++ 01-color-switcher.96eae57b.js
@@ -0,0 +1,2 @@
+!function(){var t={bodyEl:document.querySelector("body"),btnStartEl:document.querySelector("[data-start]"),btnStopEl:document.querySelector("[data-stop]")};addEventListener("click",(function(o){console.log(o.target===t.btnStartEl),t.bodyEl.style.backgroundColor="#fa3a00"}))}();
+//# sourceMappingURL=01-color-switcher.96eae57b.js.map
