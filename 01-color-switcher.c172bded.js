d0b74faf7d415c9dfbc44e7d4f8d80067a4b5806
VolZa <vvolza@gmail.com>
2023-03-03 19:13:34 +0000

    Deploying to gh-pages from @ VolZa/goit-js-hw-09@5d6c61a38cd81e461cd115a4e02db25db1512fa2 🚀

--- 01-color-switcher.c172bded.js
+++
@@ -1,2 +0,0 @@
-const t={bodyEl:document.querySelector("body"),btnStartEl:document.querySelector("[data-start]"),btnStopEl:document.querySelector("[data-stop]")};addEventListener("click",(e=>{console.log(e.target===t.btnStartEl),t.bodyEl.style.backgroundColor="#fa3a00"}));
-//# sourceMappingURL=01-color-switcher.c172bded.js.map
