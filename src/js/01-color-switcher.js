5d6c61a38cd81e461cd115a4e02db25db1512fa2
VolZa <vvolza@gmail.com>
2023-03-03 21:13:06 +0200

    hw09 task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,7 +4,29 @@ const refs = {
     btnStopEl: document.querySelector('[data-stop]'),
 }
 
+refs.btnStopEl.disabled = true;
+
+let setIntervalColor = null;
+
+const getRandomColor = function() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const showRandomColors = function() {
+  refs.bodyEl.style.backgroundColor = getRandomColor();
+}
+
+
 addEventListener('click', (ev) => {
-    console.log(ev.target===refs.btnStartEl);
-    refs.bodyEl.style.backgroundColor = '#fa3a00';
+    if (ev.target === refs.btnStartEl) {
+        console.log('start');
+        refs.btnStartEl.disabled = true;
+        refs.btnStopEl.disabled = false;
+        setIntervalColor = setInterval(() => { showRandomColors() }, 1000);
+    } else if (ev.target === refs.btnStopEl) {
+        console.log('stop');
+        refs.btnStartEl.disabled = false;
+        refs.btnStopEl.disabled = true;
+        clearInterval(setIntervalColor);
+    }
 })
